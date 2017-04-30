14a185f63e8e2f4c43c59b6832661ecc18990a4e
stereobooster <stereobooster@gmail.com>
2017-04-30 19:23:44 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+To reproduce bug
+
+```
+yarn install
+cd node_modules/react-map-gl/
+yarn install
+npm run build-es5
+npm run build-es6
+cd ../..
+yarn start
+```
---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+// import mapboxgl from 'mapbox-gl/dist/mapbox-gl-dev';
+
+import MapGL from 'react-map-gl/dist-es6/index.js'
+// import MapGL from 'react-map-gl/src/index.js'
+// import MapGL from 'mapbox-gl/dist/mapbox-gl';
+// import { default as Marker } from './components/Marker';
+
+//   <Marker
+//     latitude={37.7577}
+//     longitude={-122.4376}
+//   />
+
+// <MapGL
+//   width={400}
+//   height={400}
+//   latitude={37.7577}
+//   longitude={-122.4376}
+//   zoom={8}
+//   onChangeViewport={viewport => {
+//     // const {latitude, longitude, zoom} = viewport;
+//     // Optionally call `setState` and use the state to update the map.
+//   }}
+// >
+// </MapGL>
+
+class App extends Component {
+  render() {
+    return (
+      <div>hello world</div>
+    );
+  }
+}
+
+export default App;
